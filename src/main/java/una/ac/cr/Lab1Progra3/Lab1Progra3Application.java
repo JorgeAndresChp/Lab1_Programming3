898853e0f73446c1399b95ac5505c0a44511ae7e
Samuel Alonso Picado Marin <samipikdo@gmail.com>
2025-09-08 17:31:08 -0600

    Initial Spring Boot project setup

---
+++ src/main/java/una/ac/cr/Lab1Progra3/Lab1Progra3Application.java
@@ -0,0 +1,13 @@
+package una.ac.cr.Lab1Progra3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1Progra3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Progra3Application.class, args);
+	}
+
+}
---
+++ src/test/java/una/ac/cr/Lab1Progra3/Lab1Progra3ApplicationTests.java
@@ -0,0 +1,13 @@
+package una.ac.cr.Lab1Progra3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1Progra3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
